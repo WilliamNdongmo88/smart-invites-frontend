bd32b7f69b38460f8a752a86e296174f13ac8b13
williamndongmo88 <williamndongmo88@gmail.com>
2025-12-13 02:17:30 +0100

    feat: Optimisation (Gestion des caches)

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { environment } from '../../environment/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay, startWith, Subject, switchMap, tap } from 'rxjs';
 
 // export interface NotificationConfig {
 //   title: string;
@@ -35,6 +35,9 @@ export class NotificationService {
   // notifications = signal<Notification[]>([]);
   private apiUrl: string | undefined;
   private isProd = environment.production;
+
+  private cache$?: Observable<Notifications[]>;
+  private refresh$ = new Subject<void>();
 
   constructor(private http: HttpClient) { 
     // Définir l'URL de l'API selon l'environnement
@@ -127,16 +130,42 @@ export class NotificationService {
   //   });
   // }
 
+  // getNotifications(): Observable<Notifications[]> {
+  //   return this.http.get<Notifications[]>(`${this.apiUrl}/notification/notifications`);
+  // }
   getNotifications(): Observable<Notifications[]> {
-    return this.http.get<Notifications[]>(`${this.apiUrl}/notification/notifications`);
+    return this.refresh$.pipe(
+      startWith(void 0), // première charge
+      switchMap(() => {
+        if (!this.cache$) {
+          console.log('NOTIFICATION API CALL');
+
+          this.cache$ = this.http
+            .get<Notifications[]>(`${this.apiUrl}/notification/notifications`)
+            .pipe(shareReplay(1));
+        }
+        console.log('CACHE NOTIFICATION CALL');
+        return this.cache$;
+      })
+    );
+  }
+
+  clearNotificationsCache() {
+    this.cache$ = undefined;
   }
 
   updateNotificationReading(notifId: number, isRead: boolean): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/notification/read/${notifId}`, {isRead} );
+    return this.http.put<any>(`${this.apiUrl}/notification/read/${notifId}`, {isRead} )
+    .pipe(
+      tap(() => this.clearNotificationsCache())
+    );
   }
 
   deleteNotificationReading(notifId: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/notification/delete/${notifId}`);
+    return this.http.delete<any>(`${this.apiUrl}/notification/delete/${notifId}`)
+    .pipe(
+      tap(() => this.clearNotificationsCache())
+    );
   }
 }
 

70d5ad55bed3d19a22c048878cabb3e8a8928296
williamndongmo88 <williamndongmo88@gmail.com>
2026-02-19 06:49:18 +0100

    feat: Misa a jour du style de certaines pages

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false, // Change to true to activate production environment
   apiUrlDev: 'http://localhost:3000/api',
   // apiUrlDev: 'http://localhost:8073/api',
-  apiUrlProd:'https://smart-invites-production.up.railway.app/api'
+  // apiUrlProd:'https://smart-invites-production.up.railway.app/api',
+  apiUrlProd:'https://api.smart-invite.com/api'
 };
 if (environment.production) {
   console.log("✅ Environment de Prduction chargé !");
